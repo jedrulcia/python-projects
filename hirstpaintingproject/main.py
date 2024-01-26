f05343be728dd712dfa8e57bff41c5ae8faa533d
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-01-26 01:09:25 +0100

    adding hirstpaintingproject

--- hirstpaintingproject/main.py
+++ hirstpaintingproject/main.py
@@ -1,7 +1,6 @@
 import colorgram
 import turtle as t
 import random
-
 def extract_colors():
     colors = colorgram.extract("image.jpg", 30)
     rgb_colors = []
@@ -12,26 +11,26 @@ def extract_colors():
         rgb_colors.append((r, g, b))
     return rgb_colors
 
+def draw_dot():
+    timmy.color(random.choice(rgb_colors))
+    timmy.dot()
+    timmy.penup()
+    timmy.forward(50)
+    timmy.pendown()
+
 
 rgb_colors = extract_colors()
-
 timmy = t.Turtle()
 timmy.width(10)
 t.colormode(255)
-timmy.speed(30)
-
+timmy.speed(100)
 
 x = 0
 for _ in range(13):
     timmy.teleport(-300,-300 + x)
     x += 50
     for _ in range(13):
-        timmy.color(random.choice(rgb_colors))
-        timmy.dot()
-        timmy.penup()
-        timmy.forward(50)
-        timmy.pendown()
-
+        draw_dot()
 
 screen = t.Screen()
 screen.exitonclick()
