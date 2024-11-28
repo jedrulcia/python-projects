d36085c2507b2c963cd7a67c166fcfc09eb665a3
jedrulcia <jedrzej.koriat@gmail.com>
2024-11-28 20:08:45 +0100

    adding modified passwordmanager project

--- passwordManager/main.py
+++ passwordManager/main.py
@@ -2,68 +2,112 @@ import tkinter
 import random
 import string
 import pyperclip
+import json
+from tkinter import messagebox
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 all_characters = string.ascii_letters + string.digits + string.punctuation
 
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     password = ""
     for i in range(20):
-        password+= random.choice(all_characters)
+        password += random.choice(all_characters)
     password_input.insert(0, password)
     pyperclip.copy(password)
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_password():
+
+
+# ---------------------------- SAVE CREDENTIALS ------------------------------- #
+def add_credentials():
+    website = website_input.get().lower()
+    username = username_input.get()
+    password = password_input.get()
+
+    newdata = {
+        "Username": username,
+        "Password": password
+    }
+
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        try:
+            with open("passwords.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = {}
+
+        data[website] = newdata
+        with open("passwords.json", "w") as file:
+            json.dump(data, file, indent=4)
+        messagebox.showinfo(title="Credentials saved",
+                            message=f"Credentials successfully saved\n"
+                                    f"Website: {website}\n"
+                                    f"Username: {username}\n"
+                                    f"Password: {password}.")
+
+        website_input.delete(0)
+        password_input.delete(0)
+
+
+# ---------------------------- SEARCH CREDENTIALS ------------------------------- #
+def search_credentials():
+    website = website_input.get().lower()
     try:
-        file = open("passwords.txt")
-    except FileNotFoundError:
-        file = open("passwords.txt", "w")
-        file.write("WEBSITE | USERNAME | PASSWORD\n")
-    with open("passwords.txt", "a") as file:
-        file.write(f"{website_input.get()} | {username_input.get()} | {password_input.get()}\n")
+        with open("passwords.json", "r") as file:
+            data = json.load(file)
+            credentials = data[website]
+            messagebox.showinfo(title=f"Your credentials",
+                                message=f"Website: {website}\n"
+                                        f"Username: {credentials["Username"]}\n"
+                                        f"Password: {credentials["Password"]}")
+    except (FileNotFoundError, KeyError):
+        messagebox.showinfo(title=f"Credentials error", message="You haven't got any credentials for this webiste")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+
+# Window
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(padx=30, pady=20)
+window.config(padx=50, pady=50)
 
-window.grid_columnconfigure(3, weight=1)
-window.grid_rowconfigure(1, minsize=30)
-window.grid_rowconfigure(2, minsize=30)
-window.grid_rowconfigure(3, minsize=30)
-window.grid_rowconfigure(4, minsize=30)
-
+# Canvas
 canvas = tkinter.Canvas(width=200, height=200)
-canvas.grid(row=0, column =3)
-
 photo = tkinter.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=photo)
+canvas.grid(row=0, column=1)
 
-canvas.create_image(100,100, image=photo)
-
+# Labels
 website_label = tkinter.Label(text="Website: ")
-website_label.grid(row =1, column=2)
+website_label.grid(row=1, column=0)
 
 username_label = tkinter.Label(text="Email/Username: ")
-username_label.grid(row = 2, column = 2)
+username_label.grid(row=2, column=0)
 
 password_label = tkinter.Label(text="Password: ")
-password_label.grid(row = 3, column = 2)
+password_label.grid(row=3, column=0)
 
-website_input = tkinter.Entry(width=50)
-website_input.grid(row = 1, column = 3, columnspan =2, sticky="ew")
+# Inputs
+website_input = tkinter.Entry(width=21)
+website_input.grid(row=1, column=1)
+website_input.focus()
 
-username_input = tkinter.Entry()
-username_input.grid(row = 2, column = 3, columnspan =2, sticky="ew")
+username_input = tkinter.Entry(width=35)
+username_input.grid(row=2, column=1, columnspan=2)
+username_input.insert(0, "user@gmail.com")
 
-password_container = tkinter.Frame()
-password_container.grid(row=3, column =3, columnspan=2, sticky="ew")
+password_input = tkinter.Entry(width=21)
+password_input.grid(row=3, column=1)
 
-password_input = tkinter.Entry(password_container)
-password_input.pack(side="left", expand=True, padx=(0, 10))
+# Buttons
+search_button = tkinter.Button(text="Search", width=13, command=search_credentials)
+search_button.grid(row=1, column=2)
 
-generate_password_button = tkinter.Button(password_container, text="Generate Password", command=generate_password)
-generate_password_button.pack(side="right", expand=True)
+generate_password_button = tkinter.Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
 
-add_button = tkinter.Button(text="Add", command=add_password)
-add_button.grid(row = 4, column = 3, columnspan =2, sticky="ew")
+add_button = tkinter.Button(text="Add", width=36, command=add_credentials)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
